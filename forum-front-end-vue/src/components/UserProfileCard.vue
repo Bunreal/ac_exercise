8017c853a5f48d7b06f4be0d3c9a655963ef2cd5
Bunreal <bunreal0@gmail.com>
2022-02-06 13:59:42 +0800

    a12 with userPorfileCard finished and use dummydata

--- forum-front-end-vue/src/components/UserProfileCard.vue
+++ forum-front-end-vue/src/components/UserProfileCard.vue
@@ -2,19 +2,19 @@
   <div class="card mb-3">
     <div class="row no-gutters">
       <div class="col-md-4">
-          <img src="https://via.placeholder.com/300" width="300px" height="300px">
+          <img :src="userData.image" width="300px" height="300px">
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">user2</h5>
+          <h5 class="card-title">{{ userData.name }}</h5>
           <p class="card-text">
-            user2@example.com
+            {{ userData.email }}
           </p>
           <ul class="list-unstyled list-inline">
-            <li><strong>33</strong> 已評論餐廳</li>
-            <li><strong>2</strong> 收藏的餐廳</li>
-            <li><strong>0</strong> followings (追蹤者)</li>
-            <li><strong>1</strong> followers (追隨者)</li>
+            <li><strong> {{ userData.Comments.length }} </strong> 已評論餐廳</li>
+            <li><strong> {{ userData.FavoritedRestaurants.length }} </strong> 收藏的餐廳</li>
+            <li><strong> {{ userData.Followings.length }} </strong> followings (追蹤者)</li>
+            <li><strong> {{ userData.Followers.length }} </strong> followers (追隨者)</li>
           </ul>
           <p>
                 </p><form action="/following/3" method="POST" style="display: contents;"><button type="submit" class="btn btn-primary">追蹤</button></form>
@@ -24,3 +24,27 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    userData: {
+      type: Object,
+      default: () => ({
+        'id': -1,
+        'name': '',
+        'email': '',
+        'password': '',
+        'isAdmin': true,
+        'image': '',
+        'createdAt': '',
+        'updatedAt': '',
+        'Comments':[],
+        'FavoritedRestaurants':[],
+        'Followers': [],
+        'Followings': []
+      })
+    }
+  }
+}
+</script>
